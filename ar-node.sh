ae28b1648a9e81160fd0c6e8fbd6d4eb67712fde
Node Army <109174478+sipalingnode@users.noreply.github.com>
2023-09-27 05:16:12 +0700

    Update ar-node.sh

--- ar-node.sh
+++ ar-node.sh
@@ -20,20 +20,22 @@ echo -e "\e[1m\e[32m1. Install docker \e[0m" && sleep 1
 sudo apt update; sudo apt upgrade -y
 sudo apt-get update && sudo apt install jq git certbot nginx sqlite3 build-essential -y && sudo apt install apt-transport-https ca-certificates curl software-properties-common -y && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - && sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-echo -e "\e[1m\e[32m1. Install Nodejs & Yarn \e[0m" && sleep 1
+echo -e "\e[1m\e[32m2. Install Nodejs \e[0m" && sleep 1
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 source ~/.bashrc
 nvm install 16.15.1
 nvm use 16.15.1
+
+echo -e "\e[1m\e[32m3. Install Yarn \e[0m" && sleep 1
 curl -sSL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update -y
 sudo apt-get install yarn -y
 
-echo -e "\e[1m\e[32m1. Open Port \e[0m" && sleep 1
+echo -e "\e[1m\e[32m4. Open Port \e[0m" && sleep 1
 sudo ufw allow ssh; sudo ufw allow 80; sudo ufw allow 443; sudo ufw enable
 
-echo -e "\e[1m\e[32m1. Clone Repo \e[0m" && sleep 1
+echo -e "\e[1m\e[32m5. Clone Repo \e[0m" && sleep 1
 git clone https://github.com/ar-io/ar-io-node.git
 
 echo '=============== DONE ==================='
